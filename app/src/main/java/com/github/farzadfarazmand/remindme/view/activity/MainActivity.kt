da1e70c1e6deddf91fc79697ad3544da6eebb1b7
Farzad Farazmand <farzad.farazmand@gmail.com>
2019-06-09 11:56:04 +0430

    -add room library

--- app/src/main/java/com/github/farzadfarazmand/remindme/view/activity/MainActivity.kt
+++ app/src/main/java/com/github/farzadfarazmand/remindme/view/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.github.farzadfarazmand.remindme
+package com.github.farzadfarazmand.remindme.view.activity
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.github.farzadfarazmand.remindme.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
