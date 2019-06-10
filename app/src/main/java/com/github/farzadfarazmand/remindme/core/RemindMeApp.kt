3d70f898bcd41b3a71bd81aa0694973f06c89911
Farzad Farazmand <farzad.farazmand@gmail.com>
2019-06-10 09:06:34 +0430

    -add logger library

---
+++ app/src/main/java/com/github/farzadfarazmand/remindme/core/RemindMeApp.kt
@@ -0,0 +1,30 @@
+package com.github.farzadfarazmand.remindme.core
+
+import android.app.Application
+import com.github.farzadfarazmand.remindme.BuildConfig
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
+
+
+class RemindMeApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(false)
+            .tag("RemindMe!")
+            .build()
+
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                //show log only in debug mode
+                return BuildConfig.DEBUG
+            }
+        })
+
+
+    }
+
+}
